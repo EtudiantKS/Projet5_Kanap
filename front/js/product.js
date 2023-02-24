deb74110cf56f206dba5aaf897215aa218b8d2ff
kelly Simon <simon.kelly0820@gmail.com>
2023-02-24 14:27:41 +0100

    Recupération de l'ID du produit

--- front/js/product.js
+++ front/js/product.js
@@ -6,3 +6,19 @@
 function getProductId() {
     return new URL(location.href).searchParams.get('id')
 }
+
+const idProduct = getProductId()
+
+//Fonction qui permet de récupérer les informations du produit selon son ID //
+fetch(`http://localhost:3000/api/products/${idProduct}`) // => URL pour récuperer les valeurs souhaités 
+        .then(function (response) {  // retour positive de la promesse
+            return response.json()  // récupération du résultat de la requête au format json 
+        })
+        .then(function (products) {  // récupèration de la valeur du résultat json précédent 
+            return products
+            //return console.table(products)
+        })
+        .catch(function (error) {  // Retour négatif de la promesse
+            error = `Un problème est survenu lors du chargement, veuillez rafraîchir la page.`; // Affichage du message d'erreur
+            alert(error);
+        })
