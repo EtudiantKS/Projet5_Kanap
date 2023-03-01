e7a5d5d983535963e6cd60c642d7c8b899da5919
kelly Simon <simon.kelly0820@gmail.com>
2023-03-01 10:15:17 +0100

    commentaires (page-produit)

--- front/js/product.js
+++ front/js/product.js
@@ -3,22 +3,25 @@
 **********************************************************************************/
 
 //Fonction qui permet de récupérer l'ID du produit //
-function getProductId() {
-    return new URL(location.href).searchParams.get('id')
+function getProductId() { 
+    return new URL(location.href).searchParams.get('id') //l'objet newURL et searchParams permettent de recupérer l'id du produit
 }
 
-const idProduct = getProductId()
+//déclaration de la constante pour distinguer l'ID du produit//
+const idProduct = getProductId() 
 
 //Fonction qui permet de récupérer les informations du produit selon son ID //
-fetch(`http://localhost:3000/api/products/${idProduct}`) // => URL pour récuperer les valeurs souhaités 
-        .then(function (response) {  // retour positive de la promesse
+fetch(`http://localhost:3000/api/products/${idProduct}`) // => chemin de la ressource qu'on souhaite récupérer avec l'id du produit
+        .then(function (response) {  // retour positive 
             return response.json()  // récupération du résultat de la requête au format json 
         })
         .then(function (products) {  // récupèration de la valeur du résultat json précédent 
             return products
             //return console.table(products)
         })
-        .catch(function (error) {  // Retour négatif de la promesse
+        .catch(function (error) {  // Retour négatif 
             error = `Un problème est survenu lors du chargement, veuillez rafraîchir la page.`; // Affichage du message d'erreur
             alert(error);
         })
+
+//Fonction pour insérer les informations du produit //
