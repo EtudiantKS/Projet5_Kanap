73e00660d511268c5bbe8deb47a1fc286b53eacb
kelly Simon <simon.kelly0820@gmail.com>
2023-03-09 15:34:51 +0100

    Récupération du local Storage dans la page panier

---
+++ front/js/cart.js
@@ -0,0 +1,8 @@
+/*********************************************************************************
+Afficher un tableau récapitulatif des achats
+**********************************************************************************/
+//Récupération des produits du Local Storage sous forme de tableau
+
+let localStoragepanier = JSON.parse(localStorage.getItem("panier"));
+console.log(localStoragepanier)
+
