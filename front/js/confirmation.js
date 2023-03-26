9b4138f5a07574b770b889d0558dd95906a0d90c
kelly Simon <simon.kelly0820@gmail.com>
2023-03-26 21:30:25 +0200

    confirmation : localStorage vidé

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -7,3 +7,6 @@ let orderId = new URLSearchParams(window.location.search).get('orderId');
 
 // Le numéro de commande est créé dans le HTML et il s'affiche dans la page confirmation
 document.getElementById('orderId').innerHTML = orderId;
+
+// Le localStorage est vidé suite à la commande de l'utilisateur 
+window.localStorage.clear();
